bc813165e34c269ff0a342c1e7d0a02cbc802401
berhanez <birhanez@gmail.com>
2023-02-12 12:45:41 +0300

    datetime for init of base model

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represents the User of the HBnB project."""
+    """Represents the User of the platform"""
     
     email = ""
     password = ""
